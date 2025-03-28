9323a2d0cbe2bac5696a7613dc9f0a9f4508cfe8
dfcut8 <dfcut8@proton.me>
2025-03-28 09:54:04 -0400

    make it move

--- Assets/MainCarManager.cs
+++ Assets/MainCarManager.cs
@@ -2,6 +2,9 @@ using UnityEngine;
 
 public class MainCarManager : MonoBehaviour
 {
+    [SerializeField] float steerSpeed = 0.5f;
+    [SerializeField] float moveSpeed = 0.02f;
+
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
@@ -11,6 +14,10 @@ public class MainCarManager : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        float steerAmount = Input.GetAxis("Horizontal");
+        transform.Rotate(0, 0, steerSpeed * -steerAmount);
         
+        float moveAmount = Input.GetAxis("Vertical");
+        transform.Translate(0, moveSpeed * moveAmount, 0);
     }
 }
