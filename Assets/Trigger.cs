d2ca328f818db6248219e166f2dceaec47af3346
dfcut8 <dfcut8@proton.me>
2025-03-28 10:52:37 -0400

    assets

--- Assets/Trigger.cs
+++ Assets/Trigger.cs
@@ -2,8 +2,30 @@ using UnityEngine;
 
 public class Trigger : MonoBehaviour
 {
+    SpriteRenderer spriteRenderer;
+
+    void Start()
+    {
+        spriteRenderer = gameObject.GetComponent<SpriteRenderer>();
+    }
+
     private void OnTriggerEnter2D(Collider2D other)
     {
         Debug.Log($"Trigger detected. Name: {other.gameObject.name}");
+
+        if (spriteRenderer != null)
+        {
+            // Change the color of the sprite to red
+            spriteRenderer.color = Color.red;
+        }
+    }
+
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+        if (spriteRenderer != null)
+        {
+            // Change the color of the sprite to red
+            spriteRenderer.color = Color.white;
+        }
     }
 }
